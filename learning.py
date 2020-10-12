80ef3bfd334b87c2cb9ee5f1a886cfb23c12b443
Sarvesh Singh <sarvesh.singh@decurtis.com>
2020-10-12 08:03:45 +0530

    learning

--- learning.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-import args
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--inputString", default='Type What you want to print', type=str, required=True,
-                    help="Input String")
-input = args.inputString
-print(input)
