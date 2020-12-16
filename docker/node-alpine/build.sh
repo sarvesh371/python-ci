eca69b5e4e1af7a7595b5d2bfa63053ba8d4d49a
sarvesh.singh <sarvesh.singh@cyware.com>
2020-12-17 00:09:51 +0530

    adding teardown

--- docker/node-alpine/build.sh
+++ docker/node-alpine/build.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/sarvesh371/node-api-service.git
 image=$(cat Dockerfile | grep "IMAGE_NAME" | cut -d "=" -f2 | sed s/\'//g)
-docker build --tag $image .
+docker build --tag $image --no-cache .
